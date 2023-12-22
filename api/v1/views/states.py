b8695ec00d41596d1d846e55bc656f5620bb2884
fitowashere <fito_washere@icloud.com>
2023-12-22 19:26:35 -0400

    test 2

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
-""" objects that handle all default RestFul API actions for States """
+"""Script to handle all default RESTful API"""
+
+
 from models.state import State
 from models import storage
 from api.v1.views import app_views
-from flask import abort, jsonify, make_response, request
+from flask import jsonify, abort, make_response, request
+from flasgger.utils import swag_from
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
+@swag_from('/documentation/state/get_state.yml', methods=['GET'])
 def get_states():
-    """
-    Retrieves the list of all State objects
-    """
-    all_states = storage.all(State).values()
+    """"Get all objects from State"""
+
+    obj_states = storage.all(State).values()
     list_states = []
-    for state in all_states:
+    for state in obj_states:
         list_states.append(state.to_dict())
     return jsonify(list_states)
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/state/get_id_state.yml', methods=['get'])
 def get_state(state_id):
-    """ Retrieves a specific State """
+    """ Raises error if not linked to any State """
+
     state = storage.get(State, state_id)
     if not state:
         abort(404)
@@ -30,10 +35,9 @@ def get_state(state_id):
 
 @app_views.route('/states/<state_id>', methods=['DELETE'],
                  strict_slashes=False)
+@swag_from('documentation/state/delete_state.yml', methods=['DELETE'])
 def delete_state(state_id):
-    """
-    Deletes a State Object
-    """
+    """Deletes a State Object"""
 
     state = storage.get(State, state_id)
 
@@ -47,10 +51,10 @@ def delete_state(state_id):
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
+@swag_from('documentation/state/post_state.yml', methods=['POST'])
 def post_state():
-    """
-    Creates a State
-    """
+    """Creates a State"""
+
     if not request.get_json():
         abort(400, description="Not a JSON")
 
@@ -64,10 +68,10 @@ def post_state():
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+@swag_from('documentation/state/put_state.yml', methods=['PUT'])
 def put_state(state_id):
-    """
-    Updates a State
-    """
+    """Updates a State"""
+
     state = storage.get(State, state_id)
 
     if not state:
