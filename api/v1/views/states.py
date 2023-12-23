cba43a8f30c9c2737a31eac172a0e197ce7d5cc5
fitowashere <fito_washere@icloud.com>
2023-12-22 20:38:30 -0400

    updating states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,7 +8,7 @@ from api.v1.views import app_views
 app = Flask(__name__)
 
 
-@app_views.route('/api/v1/states', methods=['GET'])
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
     states = storage.all(State).values()
     state_list = []
@@ -17,7 +17,7 @@ def get_states():
     return jsonify(state_list)
 
 
-@app_views.route('/api/v1/states/<state_id>', methods=['GET'])
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
     state = storage.get(State, state_id)
     if state is None:
@@ -25,7 +25,7 @@ def get_state(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route('/api/v1/states/<state_id>', methods=['DELETE'])
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
 def delete_state(state_id):
     state = storage.get(State, state_id)
     if state is None:
@@ -35,7 +35,7 @@ def delete_state(state_id):
     return jsonify({}), 200
 
 
-@app_views.route('/api/v1/states', methods=['POST'])
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
 def post_state():
     if not request.get_json():
         abort(400, description="Not a JSON")
@@ -47,7 +47,7 @@ def post_state():
     return jsonify(instance.to_dict()), 201
 
 
-@app_views.route('/api/v1/states/<state_id>', methods=['PUT'])
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def put_state(state_id):
     state = storage.get(State, state_id)
     if not state:
